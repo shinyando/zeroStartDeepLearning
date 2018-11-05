168eca8f1f0e2463b75ff5b8852ae8f07ce73122
shinyando <pattsan.shinya@gmail.com>
2018-11-05 13:08:51 +0900

    3章5から終わりまでコミット

--- zeroStartDeepLearning_3.py
+++ zeroStartDeepLearning_3.py
@@ -194,3 +194,126 @@ network = init_network()
 x = np.array([1.0,0.5])
 y = forward(network,x)
 print(y)
+
+
+#3.5.1恒等関数とソフトマックス関数
+a=np.array([0.3,2.9,4.0])
+
+exp_a=np.exp(a) #指数関数
+print(exp_a)
+
+sum_exp_a=np.sum(exp_a) #指数関数の和
+print(sum_exp_a)
+
+y=exp_a/sum_exp_a
+print(y)
+
+#ソフトマックス関数の関数化 #3.5.2の注意点を考慮済み
+def softmax(a):
+    c=np.max(a)
+    exp_a=np.exp(a-c)
+    sum_exp_a=np.sum(exp_a)
+    y = exp_a/sum_exp_a
+    return y
+
+#3.5.2ソフトマックス関数の実装上の注意
+a = np.array([1010,1000,990])
+#print(np.exp(a) / np.sum(np.exp(a))) #ソフトマックス関数の計算 → 正しく計算されない
+
+c=np.max(a) #今回は1010
+print(a-c) #[0,-10,-20]
+
+print(np.exp(a-c) / np.sum(np.exp(a-c)))
+
+#3.5.3ソフトマックス関数の特徴
+a=np.array([0.3,2.9,4.0])
+y=softmax(a)
+print(y)
+print(np.sum(y)) #合計が1となり、「確率」として捉えることができる
+
+#3.6.1 MNISTデータセット
+#MNIST:機械学習分野で最も有名なデータセット
+
+(x_train,t_train), (x_test,t_test) = \
+    load_mnist(flatten=True,normalize=False)
+
+#それぞれのデータの形状を出力
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
+
+
+def img_show(img):
+    pil_img=Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train,t_train), (x_test,t_test) = \
+    load_mnist(flatten=True,normalize=False) #flatten=Truerとすると1次元で画像が格納される
+
+img=x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+
+img=img.reshape(28,28) #NumPyの配列の形状の変形を行なっている
+print(img.shape)
+
+img_show(img)
+
+#3.6.2ニューラルネットワークの推論処理
+def get_data():
+    (x_train,t_train), (x_test,t_test) = \
+    load_mnist(normalize=True,flatten=True,one_hot_label=False)
+    return x_test,t_test
+
+def init_network():
+    with open("sample_weight.pkl",'rb') as f:
+        network = pickle.load(f)
+    
+    return network
+
+def predict(network,x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    
+    a1=np.dot(x,W1)+b1
+    z1=sigmoid(a1)
+    a2=np.dot(z1,W2)+b2
+    z2=sigmoid(a2)
+    a3=np.dot(z2,W3)+b3
+    y=identity_function(a3)
+    
+    return y
+
+x,t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network,x[i])
+    p = np.argmax(y) #最も確率の高い要素のインデックスを取得
+    if p == t[i]:
+        accuracy_cnt +=1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+
+#3.6.3バッチ処理
+#大きな配列を使用することで、計算の速度が上がるから便利
+#変更点は以下の部分だけ
+
+x,t = get_data()
+network = init_network()
+
+batch_size = 100 #バッチの数
+accuracy_cnt = 0
+for i in range(0,len(x),batch_size): #range(srart,end,step)だと1項目と2項目の差がstepになる
+    x_batch=x[i:i+batch_size]#入力データのi番目からi+batch_n番目までのデータを取り出す
+    y_batch = predict(network,x_batch)
+    p = np.argmax(y_batch,axis=1)#配列のaxis次元目の要素ごとに最大値のインデックスを見つける
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
